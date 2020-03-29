21ef43383e1525b172be30237cf576b55ca5493e
JenrruPro <50652828+JenrruPro@users.noreply.github.com>
2020-03-28 21:33:35 -0700

    Update mailSender.java

--- mail/src/mailSender.java
+++ mail/src/mailSender.java
@@ -16,7 +16,9 @@ import javax.swing.JOptionPane;
 public class mailSender {
 
     String correo, contra, para, mensaje;
-
+    /*El constructor añade los valores a las variables que se usaran para enviar el mail, cuando se haga una referencia a esta 
+    clase es porque queremos enciar un mail y en los valores que se pasen por parametro al constructor seran la informacion del 
+    usuario interesado en enviar el mail*/
     public mailSender(String correo, String contra, String para, String mensaje) {
         this.correo = correo;
         this.contra = contra;
@@ -25,6 +27,8 @@ public class mailSender {
     }
 
     public void enviar() {
+        
+        //Se hacen los ajustes necesarios para enviar el mail
         Properties propies = new Properties();
 
         propies.put("mail.smtp.auth", "true");
@@ -32,7 +36,7 @@ public class mailSender {
         propies.put("mail.smtp.host", "smtp.gmail.com");
         propies.put("mail.smtp.port", "587");
 
-        Session sesion = Session.getInstance(propies, new Authenticator() {
+        Session sesion = Session.getInstance(propies, new Authenticator() { //Se crea la sesion del usuario que quiere enviar el mail
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(correo, contra);
@@ -41,7 +45,8 @@ public class mailSender {
         });
 
         MimeMessage msg = new MimeMessage(sesion);
-        try {
+        
+        try { //Se intenta enviar el mail
             JOptionPane.showMessageDialog(null, "Enviando... Esto puede tardar");
             msg.setFrom(new InternetAddress(correo));
             msg.addRecipient(Message.RecipientType.TO, new InternetAddress(para));
